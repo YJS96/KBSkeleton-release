16bc11e1089860df729f36f9d0e7863b16e8fe41
YJS96 <wnt9864@hotmail.com>
2024-06-14 22:53:20 +0900

    Analytics, 404Error

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <div>
     <NavBar />
     <RouterView />
+    <Analytics />
   </div>
 </template>
 
 <script setup>
 import NavBar from './components/NavBar.vue';
+import { Analytics } from '@vercel/analytics';
 </script>
 
 <style scoped></style>
