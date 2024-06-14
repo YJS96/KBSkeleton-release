377577728a7f98945b82a1a0262e66f48b82d797
YJS96 <wnt9864@hotmail.com>
2024-06-14 22:55:40 +0900

    Cancel Analytics, 404

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,11 @@
   <div>
     <NavBar />
     <RouterView />
-    <Analytics />
   </div>
 </template>
 
 <script setup>
 import NavBar from './components/NavBar.vue';
-import { Analytics } from '@vercel/analytics';
 </script>
 
 <style scoped></style>
